e5489171701c7ec9691ba25ebe363ff76b0ed4eb
Vladimir Zaycev <writer_2159@Mail.ru>
2022-04-23 14:57:43 +0500

    update 02

--- 02/test/index.test.js
+++ 02/test/index.test.js
@@ -5,12 +5,16 @@ const expect = require('chai').expect;
 describe('validParentheses', () => {
   describe('функция validParentheses', () => {
     [
-      ['(', false],
-      [')', false],
-      ['', true],
-      ['()', true],
-      ['())', false],
-      [')(()()(())', false],
+      ['(', true],
+      [')', true],
+      ['><', true],
+      ['', false],
+      ['()', false],
+      ['())', true],
+      [')(()()(())', true],
+      ['<(>)', true],
+      ['+([)', true],
+      ['([[+<>(+)]<+>])', false],
     ].forEach(([a, b]) => {
       it(`"${a}" - ${b?'false':'true'}`, () => {
       expect(testFunction(a)).to.equal(b);
